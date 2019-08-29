0845f51f31018012421974aa64e67d084ecf6941
bencoder <bencoder@googlemail.com>
2019-08-29 22:28:15 +0100

    Make only one ghost, add a timer. you die after 13 seconds

--- game.js
+++ game.js
@@ -40,6 +40,15 @@ const drawCircle = (c,r,fill=false) => {
     ctx.stroke();
     if (fill) ctx.fill();
 }
+
+const drawTimer = (time) => {
+    const r = 255;
+    const g = b = Math.floor(255 * (1-time/timeToDie));
+
+    setColor(`rgb(${r},${g},${b})`);
+    ctx.font = '48px serif'
+    ctx.fillText(time.toFixed(1), 10, 40)
+}
 function doesLineInterceptCircle(A, B, C, radius) {
     let dist;
     const v1x = B.x - A.x;
@@ -78,11 +87,11 @@ function Vec2(x,y) {
 
 
 const tps = 20; // number of ticks per second (FPS is greater and is done by interpolating from the previous movement to the next)
-const ghostTicks = 5 * tps; //13 seconds
+const timeToDie = 13; //13 seconds to die
 let accumulator = 0; //stores incrementing value (in seconds) until the next frame, when it's then decremented by 1 frame's length
 
 function interpolate(position, movementVector) {
-    return position.sub(movementVector).add(movementVector.mul(tps*accumulator));
+    return position.sub(movementVector.mul(1-tps*accumulator));
 }
 
 function Level(levelObject) {
@@ -140,7 +149,7 @@ function Level(levelObject) {
         }
     }
 
-    this.getStart = () => levelObject.start;
+    this.getStart = () => new Vec2(levelObject.start.x, levelObject.start.y);
 
     this.reset = () => {
         currentLevel = JSON.parse(JSON.stringify(levelObject));
@@ -175,21 +184,15 @@ function Level(levelObject) {
     }
 }
 
-function Player({x,y}, level) {
+function Player(level) {
     const speed = 100 / tps;
     const radius = 10;
-    this.position = new Vec2(x,y);
+    this.position = level.getStart()
     this.movementVector = new Vec2(0,0); //stores last movement vector
 
     this.draw = () => {
         setColor('green');
         drawCircle(interpolate(this.position, this.movementVector), radius, true);
-    }
-
-    //similar to ghost movement, used for replaying up to currentTick
-    this.forceMove = vector => {
-        level.interact(this.position, radius, vector);
-        this.position = this.position.add(vector);
     }
 
     this.move = (buttons) => {
@@ -208,40 +211,24 @@ function Player({x,y}, level) {
 }
 
 
-function Ghost(endPosition, history, level) {
-    let position = endPosition.copy();
+function Ghost(history, level) {
+    let position = level.getStart();
     const radius = 10;
-    let i;
-    for(i=history.length-1; i >= history.length-ghostTicks && i >= 0; --i) {
-        position = position.sub(history[i]);
-    }
-    const startPosition = position.copy();
-    const startTick = i+1;
-    const myHistory = history.slice(startTick);
     let movementVector = new Vec2(0,0); //stores the last movementVector
 
     this.tick = currentTick => {
-        if (currentTick < startTick) return;
-        if (currentTick-startTick > myHistory.length) return;
-        if (currentTick-startTick == myHistory.length) {
+        if (currentTick > history.length) return;
+        if (currentTick == history.length) {
             level.ghostRemoved(position, radius);
             return;
         }
-        movementVector = myHistory[currentTick - startTick];
+        movementVector = history[currentTick];
         level.interact(position, radius, movementVector);
         position = position.add(movementVector); //always apply the vector, cause we're a ghost
     }
 
-    this.reset = (currentTick) => {
-        position = startPosition.copy();
-        for (let tick=startTick; tick<=currentTick;tick++) {
-            this.tick(tick);
-        }
-    }
-
     this.draw = currentTick => {
-        if (currentTick < startTick) return;
-        if (currentTick > (startTick + myHistory.length)) return;
+        if (currentTick > history.length) return; //could draw a dead ghost(??)
         setColor('orange');
         drawCircle(interpolate(position, movementVector), radius, true);
     }
@@ -250,73 +237,53 @@ function Ghost(endPosition, history, level) {
 const Game = function(levelObject) {
     let level;
     let player;
-    let ghosts = [];
+    let ghost = null;
     let currentTick = 0;
     let history = [];
 
-    const buttons = {
-        up:false,
-        down:false,
-        left:false,
-        right:false
+    const buttons = {}
+
+    const die = () => {
+        ghost = new Ghost(history, level);
+        currentTick = 0;
+        history = [];
+
+         //reset level and player
+        level.reset();
+        player = new Player(level);
     }
 
     this.draw = () => {
         camera = interpolate(player.position, player.movementVector);
         clearScreen();
         level.draw();
-        for(g of ghosts) {
-            g.draw(currentTick);
-        }
+        if (ghost) ghost.draw(currentTick);
         player.draw();
-    }
-
-    const updateGame = () => {
-        history[currentTick] = player.move(buttons);
-        for(g of ghosts) {
-            g.tick(currentTick);
-        }
-    }
-
-    goBack = () => {
-
-        for (g of ghosts) g.reset(currentTick);
-        ghosts.push(new Ghost(player.position,history,level));
-        currentTick -= ghostTicks;  //go back in time
-        if (currentTick < 0) currentTick = 0;
-
-         //reset level and player
-        level.reset();
-        player = new Player(level.getStart(), level);
-        
-        history = history.slice(0,currentTick);
-        //play everything forward
-        for(let i=0;i<currentTick;i++) {
-            player.forceMove(history[i]);
-            for (g of ghosts) {
-                g.tick(i);
-            }
-        }
+        drawTimer(currentTick / tps);
     }
 
     this.tick = () => {
-        updateGame();
+        if (currentTick == timeToDie * tps) {
+            die();
+            return;
+        }
+        history[currentTick] = player.move(buttons);
+        if (ghost)  ghost.tick(currentTick);
         ++currentTick;
     }
 
     this.buttonDown = key => {
-        if (key in buttons) {
-            buttons[key] = true;
-        }
+        buttons[key] = true;
         if (key == 'back') {
-            goBack();
+            die();
         }
     }
+
     this.buttonUp = key => buttons[key] = false;
 
     this.loadLevel = (levelObject) => {
         level = new Level(levelObject);
-        player = new Player(levelObject.start, level);
+        player = new Player(level);
     }
 
     if (levelObject) {

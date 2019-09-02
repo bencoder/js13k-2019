fa4748c32e3c45787200fcd6767b6cd1d73a55bd
Salvatore Previti <roorback@gmail.com>
2019-09-02 10:15:21 +0100

    zip build

---
+++ scripts/lib/humanReadableFileSize.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const prettyFileSize = require('prettysize');
+
+function humanReadableFileSize(filenameOrSize) {
+	try {
+		const sz = typeof filenameOrSize === 'string' ? fs.statSync(filenameOrSize).size : filenameOrSize;
+		return `${prettyFileSize(sz)} (${sz} bytes)`;
+	} catch (e) {
+		return 'NOT FOUND!';
+	}
+}
+
+module.exports = humanReadableFileSize;

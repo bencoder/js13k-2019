aa11f4d01b8ad8b88fc37942f62142e9bbc4b7f4
Salvatore Previti <roorback@gmail.com>
2019-09-12 01:25:19 +0100

    minification

--- scripts/lib/minifyJs.js
+++ scripts/lib/minifyJs.js
@@ -1,8 +1,8 @@
-const UglifyJS = require('uglify-es')
+const terser = require('terser')
 
 function minifyJs(code) {
   console.time('minifyJs')
-  const ret = UglifyJS.minify(code, getOptions())
+  const ret = terser.minify(code, getOptions())
   if (ret.error) {
     throw new Error(ret.error)
   }
@@ -15,29 +15,39 @@ function minifyJs(code) {
 function getOptions() {
   // Documentation of the options is available at https://github.com/mishoo/UglifyJS2
   return {
+    ecma: 8,
+    keep_classnames: false,
+    keep_fnames: false,
+    ie8: false,
+    module: false,
+    safari10: false,
+    toplevel: true,
     parse: {
       bare_returns: false,
-      ecma: 9,
+      ecma: 8,
       expression: false,
       filename: null,
       html5_comments: true,
       shebang: true,
       strict: false,
-      toplevel: null
+      toplevel: true
     },
     compress: {
       arrows: true,
+      arguments: true,
       booleans: true,
+      booleans_as_integers: true,
       collapse_vars: true,
       comparisons: true,
       computed_props: true,
       conditionals: true,
+      directives: true,
       dead_code: true,
       drop_console: true,
       drop_debugger: true,
-      ecma: 9,
+      ecma: 8,
       evaluate: true,
-      expression: true,
+      expression: false,
       global_defs: {},
       hoist_funs: true,
       hoist_props: true,
@@ -47,22 +57,22 @@ function getOptions() {
       inline: true,
       join_vars: true,
       keep_classnames: false,
-      keep_fargs: true,
+      keep_fargs: false,
       keep_fnames: false,
       keep_infinity: false,
       loops: true,
       negate_iife: true,
       passes: 4,
       properties: true,
-      pure_getters: 'strict',
+      pure_getters: true,
       pure_funcs: null,
       reduce_funcs: true,
       reduce_vars: true,
       sequences: true,
       side_effects: true,
-      switches: false,
+      switches: true,
       top_retain: null,
-      toplevel: false,
+      toplevel: true,
       typeofs: true,
       unsafe: true,
       unsafe_arrows: true,
@@ -84,14 +94,13 @@ function getOptions() {
       properties: false,
       reserved: [],
       safari10: false,
-      toplevel: false
+      toplevel: true
     },
     output: {
       ascii_only: true,
       beautify: false,
-      bracketize: false,
       comments: /@license|@preserve|^!/,
-      ecma: 9,
+      ecma: 8,
       ie8: false,
       indent_level: 2,
       indent_start: 0,
@@ -99,7 +108,6 @@ function getOptions() {
       keep_quoted_props: false,
       max_line_len: false,
       preamble: null,
-      preserve_line: false,
       quote_keys: false,
       quote_style: 0,
       safari10: false,

b14b81fa0cebf4f3445c2c1d2efc7de21179439e
Seth Kitchen <sjkyv5@mst.edu>
2019-10-18 16:13:02 -0500

    add on screen controls for mobile

---
+++ src/controls.js
@@ -0,0 +1,35 @@
+//Move Up
+function startUp() {
+  game.buttonDown('up');
+}
+
+function stopUp() {
+  game.buttonUp('up');
+}
+
+//Move Left
+function startLeft() {
+  game.buttonDown('left');
+}
+
+function stopLeft() {
+  game.buttonUp('left');
+}
+
+//Move Right
+function startRight() {
+  game.buttonDown('right');
+}
+
+function stopRight() {
+  game.buttonUp('right');
+}
+
+//Move Down
+function startDown() {
+  game.buttonDown('down');
+}
+
+function stopDown() {
+  game.buttonUp('down');
+}
